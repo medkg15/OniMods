1c77f69f78c4f9eafe84b7e4f82d9730b3dc238f
medkg15 <medkg15@gmail.com>
2022-01-02 20:28:21 -0500

    Add mod for muting starving notifications.

---
+++ MuteStarvingNotification/src/MuteStarvingNotificationPatches.cs
@@ -0,0 +1,29 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using STRINGS;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MuteStarvingNotificationPatches
+{
+    [HarmonyPatch(typeof(NotificationScreen))]
+    [HarmonyPatch("PlayDingSound")]
+    public static class MuteStarvingNotificationPatches
+    {
+        public static void Prefix(
+          ref NotificationScreen __instance,
+          Notification notification)
+        {
+            if (notification.titleText == "Starvation")
+            {
+                notification.playSound = false;
+            }
+        }
+    }
+}
